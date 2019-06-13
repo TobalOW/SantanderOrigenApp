8d08c81faeb42c9dfadebe3a48cffc4225661e8d
Cristobal Miranda <cristobal.miranda.u@gmail.com>
2019-06-13 17:01:36 -0400

    update constants

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { StyleSheet, TextInput } from 'react-native'
-import { Icon } from 'expo';
+import * as Icon from '@expo/vector-icons'
 
 import Text from './Text';
 import Block from './Block';
